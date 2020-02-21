3885f89490f595af3f64abeac54ef7fc771bd4b4
atongia10 <atongia10@gmail.com>
2020-02-20 23:18:32 -0500

    Kth Largest Number In An Array - Modified Logic

--- src/main/java/ds/array/KthLargestInArray.java
+++ src/main/java/ds/array/KthLargestInArray.java
@@ -5,19 +5,20 @@ import java.util.PriorityQueue;
 public class KthLargestInArray {
     public static void main(String[] args) {
         int[] array = {1, 2, 3, 4, 5, 8, 8, 9, 9, 9, 9, 9, 10, 10, 10, 10};
-        int k = 5;
+        int k = 2;
         System.out.println(kthLargestInGivenArray(array, k));
     }
 
     public static int kthLargestInGivenArray(int[] array, int k) {
 
+        int count = 0;
         PriorityQueue<Integer> queue = new PriorityQueue<>();
         for (int i = 0; i < array.length; i++) {
-            if (!queue.contains(array[i])) {
-                queue.add(array[i]);
-                if (queue.size() > k)
-                    queue.poll();
+            if (count >= k && array[i] > queue.peek() && !queue.contains(array[i])) {
+                queue.poll();
             }
+            count++;
+            queue.add(array[i]);
         }
 
         return queue.peek();
