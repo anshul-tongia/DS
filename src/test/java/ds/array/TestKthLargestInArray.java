b9d8203a513c932a8fd43bc8131b6452c2cc64b3
atongia10 <atongia10@gmail.com>
2020-02-20 22:47:38 -0500

    Kth Largest Number In An Array - Test Case Upgrade

--- src/test/java/ds/array/TestKthLargestInArray.java
+++ src/test/java/ds/array/TestKthLargestInArray.java
@@ -6,8 +6,26 @@ import org.junit.Test;
 public class TestKthLargestInArray {
 
     @Test
-    public void test() {
+    public void test1() {
         int result = KthLargestInArray.kthLargestInGivenArray(new int[]{10, 10, 9}, 2);
         Assert.assertEquals(9, result);
+    }
+
+    @Test
+    public void test2() {
+        int result = KthLargestInArray.kthLargestInGivenArray(new int[]{1, 5, 12, 2, 11, 5}, 3);
+        Assert.assertEquals(5, result);
+    }
+
+    @Test
+    public void test3() {
+        int result = KthLargestInArray.kthLargestInGivenArray(new int[]{1, 5, 12, 2, 11, 5}, 4);
+        Assert.assertEquals(2, result);
+    }
+
+    @Test
+    public void test4() {
+        int result = KthLargestInArray.kthLargestInGivenArray(new int[]{5, 12, 11, -1, 12}, 3);
+        Assert.assertEquals(5, result);
     }
 }
